659aa32d644b4fad31845bb7000f2a1d41918fb4
bhartivikas <bhartivikas2427@gmail.com>
2023-08-10 20:55:02 +0530

    Modified main.tf file

--- JenkinsOnEc2/main.tf
+++ JenkinsOnEc2/main.tf
@@ -4,16 +4,16 @@ provider "aws" {
 
 
 resource "aws_instance" "jenkins_server" {
-  ami           = var.ami_id
+  ami = var.ami_id
   instance_type = var.instance_type
   tags = {
     Name = var.tag_name
   }
   key_name = "JenkinsOnEc2"
   connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
     private_key = file("C:/Users/2412f/.aws/JenkinsOnEc2.pem")
   }
   provisioner "remote-exec" {
@@ -37,13 +37,13 @@ resource "aws_instance" "jenkins_server" {
 
       # install jenkins
       # https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/
-      # "echo 'START: Installing Jenkins'",
-      # "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo",
-      # "sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key",
-      # "sudo yum install jenkins -y",
-      # "sudo systemctl enable jenkins",
-      # "sudo systemctl start jenkins",
-      # "echo 'END: Installing Jenkins'"
+      "echo 'START: Installing Jenkins'",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key",
+      "sudo yum install jenkins -y",
+      "sudo systemctl enable jenkins",
+      "sudo systemctl start jenkins",
+      "echo 'END: Installing Jenkins'"
     ]
   }
 }
