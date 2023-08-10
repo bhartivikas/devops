488bcbadc099626058e8b89b9ab3060fc3db62db
bhartivikas <bhartivikas2427@gmail.com>
2023-08-10 12:32:01 +0530

    Updating main.tf and variables.tf file

--- JenkinsOnEc2/main.tf
+++ JenkinsOnEc2/main.tf
@@ -1,15 +1,35 @@
 provider "aws" {
-    region = var.mumbai
-  
+  region = var.region
 }
 
 
 resource "aws_instance" "jenkins_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  
-
   tags = {
-    Name = "var.tag_name"
+    Name = var.tag_name
+  }
+  key_name = "JenkinsOnEc2"
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("C:/Users/kumar/.aws/JenkinsOnEc2.pem")
+  }
+  # https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'START: Installing Java 17'",
+      "sudo yum upgrade -y",
+      "sudo yum install java-17-amazon-corretto.x86_64 -y",
+      "echo 'END: Installing Java 17'",
+      "echo 'START: Installing Jenkins'",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key",
+      "sudo yum install jenkins -y",
+      "sudo systemctl enable jenkins",
+      "sudo systemctl start jenkins",
+      "echo 'END: Installing Jenkins'"
+    ]
   }
 }
--- JenkinsOnEc2/variables.tf
+++ JenkinsOnEc2/variables.tf
@@ -1,23 +1,19 @@
 variable "region" {
-    type = string
-    default = "ap-south-1"
-  
+  type = string
+  default = "ap-south-1"
 }
 
 variable "ami_id" {
     type = string
-    default = " ami-02bc45136ff0b128e"
-  
+    default = "ami-0ded8326293d3201b"
 }
 
 variable "instance_type" {
     type = string
     default = "t2.micro"
-  
 }
 
 variable "tag_name" {
     type = string
     default = "devops"
-  
 }
